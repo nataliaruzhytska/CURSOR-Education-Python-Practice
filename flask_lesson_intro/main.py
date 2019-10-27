a60666b92211f4d7012e27c6a0635352ae5b0549
natalia.ruzhytska@synevo.ua <natalia.ruzhytska@synevo.ua>
2019-10-28 00:47:25 +0200

    completed

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -9,5 +9,40 @@ def get_home_page():
     return render_template("home.html", data=get_data())
 
 
+@app.route('/Alarm_clock')
+def get_alarm_clock():
+    return render_template('alarm_clock.html', data=get_data())
+
+
+@app.route('/Headphones')
+def get_headphones():
+    return render_template('headphones.html', data=get_data())
+
+
+@app.route('/iPod')
+def get_ipod():
+    return render_template('ipod.html', data=get_data())
+
+
+@app.route('/Calculator')
+def get_calculator():
+    return render_template('calculator.html', data=get_data())
+
+
+@app.route('/Coffeemaker')
+def get_coffeemaker():
+    return render_template('coffeemaker.html', data=get_data())
+
+
+@app.route('/Battery_charger')
+def get_battery_charger():
+    return render_template('battery_charger.html', data=get_data())
+
+
+@app.route('/author_page')
+def get_author_page():
+    return render_template('/author_page.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)

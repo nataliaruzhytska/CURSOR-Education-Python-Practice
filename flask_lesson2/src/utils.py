0303674a2a6dc1a8dfc3d32fe17c5e8edea04ca5
natalia.ruzhytska@synevo.ua <natalia.ruzhytska@synevo.ua>
2019-11-06 11:15:03 +0200

    -

--- flask_lesson2/src/utils.py
+++ flask_lesson2/src/utils.py
@@ -6,13 +6,14 @@ def get_data(item_list):
         with open(item_list) as res:
             return json.load(res)
     except(IOError, ValueError, FileNotFoundError, json.JSONDecodeError):
-        raise
+        return []
 
 
 def add_data(data, item_list):
     try:
         with open(item_list, mode="w") as res:
             return json.dump(data, res)
-    except(IOError, FileNotFoundError, ValueError):
-        raise
+    except ValueError:
+        return item_list
+
 
